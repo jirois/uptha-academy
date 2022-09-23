30935d27fde492d77eb1acfc4d0e9e267fba1261
Omas Ajiri <omasajiri@gmail.com>
2022-09-23 11:41:32 +0100

    fix: commit first changes

--- src/App.js
+++ src/App.js
@@ -2,23 +2,18 @@ import React from "react";
 import {
   BrowserRouter as Router,
   Routes as Switch,
-  Route
-} from 'react-router-dom'
-import Footer from "./component/Footer";
-import NavbarHome from "./component/Navbar/NavbarHome";
+  Route,
+} from "react-router-dom";
 import Home from "./page";
 import Register from "./page/Register";
-
 
 function App() {
   return (
     <Router>
-      <NavbarHome />
       <Switch>
         <Route path="/" element={<Home />} exact />
         <Route path="/register" element={<Register />} exact />
       </Switch>
-      <Footer />
     </Router>
   );
 }
--- src/page/index.js
+++ src/page/index.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import Courses from '../component/Course';
-import HeroSection from '../component/Hero';
-import InfoSection from '../component/InfoSection';
-import { homeObjOne, homeObjThree, homeObjTwo } from '../component/InfoSection/data'
-
+import React from "react";
+import Courses from "../component/Course";
+import Footer from "../component/Footer";
+import HeroSection from "../component/Hero";
+import InfoSection from "../component/InfoSection";
+import {
+  homeObjOne,
+  homeObjThree,
+  homeObjTwo,
+} from "../component/InfoSection/data";
+import NavbarHome from "../component/Navbar/NavbarHome";
 
 const Home = () => {
-
-    return (
-        <>
-            <HeroSection />
-            <InfoSection {...homeObjOne} />
-            <InfoSection {...homeObjTwo} />
-            <Courses />
-            <InfoSection {...homeObjThree} />
-        </>
-    )
+  return (
+    <>
+      <NavbarHome />
+      <HeroSection />
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjTwo} />
+      <Courses />
+      <InfoSection {...homeObjThree} />
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
